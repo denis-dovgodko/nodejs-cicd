a3ca3fca3165138b0247ce5c563c3fc83f5c8fed
denis.dovgodko <dendov555@gmail.com>
2024-09-10 00:08:12 +0300

    link in test fix

--- __tests__/connection.test.js
+++ __tests__/connection.test.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 test('should get index', async () => {
-  const response = await axios.get('localhost');
+  const response = await axios.get('http://localhost');
   expect(response.status).toBe(200);
   expect(response.data).toHaveProperty('status', 'ok');
 });
